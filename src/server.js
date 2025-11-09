277fcbb7dea6d349b5a2749caf2a3642513105db
Romana Solovan <solo.rv95@gmail.com>
2025-11-09 14:10:51 +0200

    Add MongoDB and CRUD operations

--- src/server.js
+++ src/server.js
@@ -1,46 +1,30 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import pinoHttp from 'pino-http';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { connectMongoDB } from "./db/connectMongoDB.js";
+import { logger } from "./middleware/logger.js";
+import { notFoundHandler } from "./middleware/notFoundHandler.js";
+import { errorHandler } from "./middleware/errorHandler.js";
+import notesRouter from "./routes/notesRoutes.js";
 
 dotenv.config();
 
 const app = express();
+
+app.use(logger);
+app.use(express.json());
+app.use(cors());
+
+app.use("/notes", notesRouter);
+
+app.use(notFoundHandler);
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 3000;
 
-// Standard middleware
-app.use(cors());
-app.use(express.json());
+const startServer = async () => {
+  await connectMongoDB();
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+};
 
-// Logger middleware
-const logger = pinoHttp();
-app.use(logger);
-
-// Routes
-app.get('/notes', (req, res) => {
-  res.status(200).json({ message: 'Retrieved all notes' });
-});
-
-app.get('/notes/:noteId', (req, res) => {
-  const { noteId } = req.params;
-  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
-});
-
-app.get('/test-error', () => {
-  throw new Error('Simulated server error');
-});
-
-// 404 middleware
-app.use((req, res) => {
-  res.status(404).json({ message: 'Route not found' });
-});
-
-// 500 middleware
-app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message });
-});
-
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+startServer();
